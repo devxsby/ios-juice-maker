89d6b985f12b7a6eb6ffa46f3016e5d48662ae20
ueunli <cdenen20@gmail.com>
2023-01-10 19:05:26 +0900

    [feat] 대량입고 기능 추가
    - `increase(_:amount:)` 메서드로 원하는 수량만큼 한번에 재고추가
    - UIImage 또는 UIButton을 추가하여 구현할 것 (현재는 임시로 stepper에 연결)

--- JuiceMaker/JuiceMaker/Source/Screen/Store/StoreViewController.swift
+++ JuiceMaker/JuiceMaker/Source/Screen/Store/StoreViewController.swift
@@ -14,6 +14,7 @@ final class StoreViewController: UIViewController, StoreDelegate {
     private let fruitStore = FruitStore.shared
     weak var delegate: StoreDelegate?
     
+    @IBOutlet var fruitImageLabels: [UILabel]!
     @IBOutlet private var fruitStockLabels: [UILabel]!
     @IBOutlet private var fruitStockSteppers: [UIStepper]!
     
@@ -23,10 +24,15 @@ final class StoreViewController: UIViewController, StoreDelegate {
         fruitStore.storeDelegate = self
     }
     
-    @IBAction private func stockStepperDidTap(_ sender: UIStepper) {
+    @IBAction private func stockStepperDidTap(_ sender: UIImageView) {
         guard let stepper = fruitStockSteppers[safe: sender.tag] else { return }
         guard let fruit = Fruits.allCases[safe: stepper.tag] else { return }
         fruitStore.changeStock(of: fruit, count: Int(stepper.value))
+    }
+    
+    @IBAction private func fruitImageDidTap(_ sender: UIStepper) {
+        guard let fruit = Fruits.allCases[safe: sender.tag] else { return }
+        presentAlertAsking(bulkOf: fruit)
     }
     
     @IBAction private func confirmChangedStockButtonDidTap(_ sender: UIBarButtonItem) {
@@ -41,5 +47,25 @@ final class StoreViewController: UIViewController, StoreDelegate {
             label.text = String(stock)
             stepper.value = Double(stock)
         }
+    }
+    
+    private func presentAlertAsking(bulkOf fruit: Fruits) {
+        let alert = UIAlertController(title: "대량입고",
+                                      message: "추가할 수량을 입력해 주세요.",
+                                      preferredStyle: .alert)
+        alert.addTextField { field in
+            field.keyboardType = .numberPad
+            field.returnKeyType = .done
+        }
+        let confirm = UIAlertAction(title: "예", style: .default, handler: { _ in
+            guard let field = alert.textFields?[0] else { return }
+            let amount = Int(field.text ?? "0") ?? 0
+            self.fruitStore.increase(of: fruit, amount: amount)
+        })
+        let close = UIAlertAction(title: "아니요", style: .destructive)
+        
+        alert.addAction(confirm)
+        alert.addAction(close)
+        present(alert, animated: true, completion: nil)
     }
 }
